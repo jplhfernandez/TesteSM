9de60e28c6ea19835742ff711d532c2bd174c630
João Pedro <jplhfernandez@gmail.com>
2024-11-11 10:45:44 -0300

    imagem e insert

--- a/insere_produtos.php
+++ a/insere_produtos.php
@@ -1,30 +1,32 @@
 <?php 
 include 'acesso_com.php';
 include '../banco/connect.php';
-if($_POST){
+if($_POST)
+{
     if(isset($_POST['enviar'])){
         $nome_img = $_FILES['imagemfile']['name'];
         $tmp_img = $_FILES['imagemfile']['tmp_name'];
         $rand = rand(100001,999999);
-        $dir_img = "../img/ADM".$rand.$nome_img;
+        $dir_img = "../img/produtos/".$rand.$nome_img;
         move_uploaded_file($tmp_img,$dir_img);
     }
 
-    $id = $_POST['id_tipo'];
-    $destaque = $_POST['destaque'];
-    $descricao = $_POST['descricao'];
-    $resumo = $_POST['resumo'];
-    $valor = $_POST['valor'];
+    $categoria_id = $_POST['categoria_id']; 
+    $nome_produto = $_POST['nome_produto'];
     $imagem =  $rand.$nome_img;
+    $valor = $_POST['valor'];
+    $unidade_venda = $_POST['unidade_venda'];
+    $descricao = $_POST['descricao'];
+    $destaque = $_POST['destaque'];
     
     $insereProduto = "insert produtos 
-                    (tipo_id, descricao, resumo, valor, imagem, destaque)
+                    (id_categoria, nome_produto, imagem, valor, unidade_venda, descricao, destaque)
                     values
-                    ($id,'$descricao', '$resumo', $valor, '$imagem', '$destaque') 
+                    ($categoria_id,'$nome_produto', '$imagem', $valor, $unidade_venda, '$descricao', '$destaque') 
                     ";
     $resultado = $conn->query($insereProduto);            
     if(mysqli_insert_id($conn)){
-        header('lista_produto.php');
+        header('lista_produtos.php');
     }
 }
 // selecionar a lista de tipos para preencher o <select>
@@ -60,21 +62,21 @@ $numLinhas = $listaTipo->num_rows;
             </h2>
             <div class="thumbnail-insere">
                 <div class="alert alert-success" role="alert">
-                    <form action="produtos_insere.php" method="post" 
+                    <form action="insere_produtos.php" method="post" 
                     name="form_insere" enctype="multipart/form-data"
                     id="form_insere">
-                        <label for="id_tipo">Tipo:</label>
-                        <div class="input-group-text">
-                            <span class="input">
+                        <label for="categoria_id">Tipo:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
                                 <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
                                 <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8m0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0M4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
                                 </svg>
                             </span>
-                            <select name="id_tipo" id="id_tipo" class="form-control" required>
+                            <select name="categoria_id" id="categoria_id" class="form-control" required>
                                 <?php do{ ?>
                                     <option value="<?php echo $rowTipo['id']; ?>">
-                                    <?php echo $rowTipo['categorias.rotulo']; ?>
+                                    <?php echo $rowTipo['rotulo']; ?>
                                     </option>
                                 <?php }while($rowTipo = $listaTipo->fetch_assoc()); ?>
                             </select>
@@ -85,10 +87,10 @@ $numLinhas = $listaTipo->num_rows;
                                 <input type="radio" name="destaque" id="destaque" value="Sim">Sim
                             </label>
                             <label for="destaque_n" class="radio-inline">
-                                <input type="radio" name="destaque" id="destaque" value="Não" checked>Não
+                                <input type="radio" name="destaque" id="destaque" value="Nao" checked>Não
                             </label>
                         </div>
-                            <label for="nome">Nome do Produto:</label>     
+                            <label for="nome_produto">Nome do Produto:</label>     
                         <div class="input-group-text">
                            <span class="input-group-addon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
@@ -96,7 +98,7 @@ $numLinhas = $listaTipo->num_rows;
                                 <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
                                 </svg>                           
                             </span>
-                           <input type="text" name="nome" id="nome" 
+                           <input type="text" name="nome_produto" id="nome_produto" 
                                 class="form-control" placeholder="Digite o Nome do Produto"
                                 maxlength="100" required>
                         </div>   
@@ -118,9 +120,21 @@ $numLinhas = $listaTipo->num_rows;
                         <label for="valor">Valor:</label>     
                         <div class="input-group mb-3">
                             <span class="input-group-text">R$</span>
-                            <span class="input-group-text">0.00</span>
-                            <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
-                        </div>  
+                            <input type="number" name="valor" id="valor" 
+                                class="form-control" required min="0" step="0.01">
+                        </div>   
+
+                        <label for="unidade_venda">Unidade de Venda:</label>     
+                        <div class="input-group-text mb-3">
+                            <span class="input-group-text">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-123" viewBox="0 0 16 16">
+                                    <path d="M2.873 11.297V4.142H1.699L0 5.379v1.137l1.64-1.18h.06v5.961zm3.213-5.09v-.063c0-.618.44-1.169 1.196-1.169.676 0 1.174.44 1.174 1.106 0 .624-.42 1.101-.807 1.526L4.99 10.553v.744h4.78v-.99H6.643v-.069L8.41 8.252c.65-.724 1.237-1.332 1.237-2.27C9.646 4.849 8.723 4 7.308 4c-1.573 0-2.36 1.064-2.36 2.15v.057zm6.559 1.883h.786c.823 0 1.374.481 1.379 1.179.01.707-.55 1.216-1.421 1.21-.77-.005-1.326-.419-1.379-.953h-1.095c.042 1.053.938 1.918 2.464 1.918 1.478 0 2.642-.839 2.62-2.144-.02-1.143-.922-1.651-1.551-1.714v-.063c.535-.09 1.347-.66 1.326-1.678-.026-1.053-.933-1.855-2.359-1.845-1.5.005-2.317.88-2.348 1.898h1.116c.032-.498.498-.944 1.206-.944.703 0 1.206.435 1.206 1.07.005.64-.504 1.106-1.2 1.106h-.75z"/>
+                                </svg>
+                            </span>
+                            <input type="number" class="form-control" id="unidade_venda" name="unidade_venda" aria-label="Quantidade de unidade de venda" placeholder="0" min="1" step="1" required>
+                        </div>
+
+
                         <label for="imagem">Imagem:</label>    
                         <div class="input-group-text">
                             <span class="input-group-addon">
