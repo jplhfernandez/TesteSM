2dbd066e843d339c23372e5905a7b978a3219520
João Pedro <jplhfernandez@gmail.com>
2024-11-11 00:25:42 -0300

    listar pronto

--- a/lista_produtos.php
+++ a/lista_produtos.php
@@ -51,9 +51,13 @@ $rows = $lista -> num_rows;
                         <td>
                             <?php
                                 if($row['destaque'] == 'Sim') {
-                                    echo '<i class="bi bi-star-fill" aria-hidden="true"></i>';
+                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                        </svg>';
                                 } else {
-                                    echo '<i class="bi bi-brightness-alt-high-fill" aria-hidden="true"></i>';
+                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                                        </svg>';
                                 }
                                 echo '&nbsp;' . $row['nome_produto'];
                             ?>
